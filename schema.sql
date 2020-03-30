d27eb3fdd92f0326305d879950ad431516d7ba1d
= <=>
2020-03-30 08:36:58 +0200

    Update to schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ drop table if exists users;
 
 create table users (
     id int auto_increment primary key,
-    username varchar(255) unique,
-    salt varchar(16),
-    password varchar(255)
-);
+    username varchar(255) not null,
+    salt varchar(16) not null,
+    password varchar(255) not null,
+    unique key 
+) engine = InnoDB ;

d27fbfe0195d837d192fc4539e7445e4d8770dd7
= <=>
2020-03-30 08:20:47 +0200

    More changes to schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ drop table if exists users;
 
 create table users (
     id int auto_increment primary key,
-    username varchar unique,
+    username varchar(100) unique,
     salt varchar(16),
-    password varchar
+    password varchar(100)
 );

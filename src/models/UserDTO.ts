9164ab2168e26ddb53b81d54cbf178f96a9cc16a
= <=>
2020-03-30 08:01:47 +0200

    Setup for signup api

---
+++ src/models/UserDTO.ts
@@ -0,0 +1,12 @@
+
+class UserDTO {
+    username: string;
+    password: string;
+
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
+export default UserDTO
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import userController from './controllers/UsersController'
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/api', userController)
+
+app.listen(8080, () => {
+    console.log('Server running at port 8080')
+})

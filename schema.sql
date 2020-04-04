136a283e6a72bebef1438b0130de4dc1e0fa8afc
= <=>
2020-04-04 12:34:44 +0200

    Updated schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,21 @@
+drop table if exists budgets_categories;
 drop table if exists users;
+drop table if exists categories;
 
 create table users (
     id int auto_increment primary key,
     username varchar(100) not null unique,
     password varchar(255) not null
+);
+
+create table categories (
+    id int auto_increment primary key,
+    category varchar(100) not null unique
+);
+
+create table budgets_categories (
+    user_id int foreign key users(id),
+    category_id int foreign key categories(id),
+    value int,
+    primary key (user_id, category_id)
 );

ca2d6a8d6dc6bf1a83ddb4dde7dfbf01bc5452fd
Peter Moss <peter@moss.dk>
2020-04-06 10:56:38 +0200

    Updated jwt secret

--- api/middleware/check-auth.ts
+++ api/middleware/check-auth.ts
@@ -4,12 +4,12 @@ import jwt from 'jsonwebtoken'
 import { RequestHandler } from 'express'
 
 const checkAuth: RequestHandler = (req, res, next) => {
-    if (!process.env.JWT_TOKEN) return res.status(500).json({
+    if (!process.env.JWT_SECRET) return res.status(500).json({
         message: 'Server secret not set. Could not issue token!'
     })
     try {
         const token = req.headers.authorization!.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.userToken = decoded
         next()
     } catch (error) {

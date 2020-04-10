49057de9a63b410325e4ddb351c1ea7c980ac7b7
Peter Moss <peter@moss.dk>
2020-04-10 19:14:57 +0200

    Updated options response

--- server/app.ts
+++ server/app.ts
@@ -13,8 +13,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*, Authorization')
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH')
-        return res.status(200).json()
+        res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PATCH')
+        return res.status(200).send()
     }
     next()
 })

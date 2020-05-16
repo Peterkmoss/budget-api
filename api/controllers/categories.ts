d42c106d4bdb6f7e0c6c22e041d0751768d12ced
Peter Moss <peter@moss.dk>
2020-05-16 11:51:32 +0200

    Updated queries

--- api/controllers/categories.ts
+++ api/controllers/categories.ts
@@ -4,7 +4,7 @@ import { RequestHandler } from 'express'
 export const getAllCategories: RequestHandler = (req, res) => {
     pool.getConnection((err, connection) => {
         if (err) return res.status(500).json(err)
-        connection.query('select category from categories', (err, results) => {
+        connection.query('SELECT * FROM categories', (err, results) => {
             connection.release()
             if (err) return res.status(500).json(err)
             res.status(201).json({
@@ -15,11 +15,11 @@ export const getAllCategories: RequestHandler = (req, res) => {
 }
 
 export const getCategory: RequestHandler = (req, res) => {
-    const category = req.params.category
+    const category = req.params.name
 
     pool.getConnection((err, connection) => {
         if (err) return res.status(500).json(err)
-        connection.query('select category from categories where category = ?', category, (err, results) => {
+        connection.query('SELECT * FROM CATEGORIES WHERE name = ?', category, (err, results) => {
             connection.release()
             if (err) return res.status(500).json(err)
             res.status(201).json({
@@ -31,12 +31,13 @@ export const getCategory: RequestHandler = (req, res) => {
 
 export const addCategory: RequestHandler = (req, res) => {
     const category = {
-        category: req.body.category
+        name: req.body.name,
+        value: req.body.value
     }
 
     pool.getConnection((err, connection) => {
         if (err) return res.status(500).json(err)
-        connection.query('insert into categories set ?', category, (err) => {
+        connection.query('INSERT INTO categories SET ?', category, (err) => {
             connection.release()
             if (err) return res.status(500).json(err)
             res.status(201).json({
@@ -51,7 +52,7 @@ export const updateCategory: RequestHandler = (req, res) => {
 
     pool.getConnection((err, connection) => {
         if (err) return res.status(500).json(err)
-        connection.query('update categories set category = ? where category = ?', [req.body.category, category], (err, results) => {
+        connection.query('UPDATE categories SET category = ? WHERE category = ?', [req.body.category, category], (err, results) => {
             connection.release()
             if (err) return res.status(500).json(err)
             if (!(results['affectedRows'] > 0)) return res.status(404).json({
@@ -69,7 +70,7 @@ export const deleteCategory: RequestHandler = (req, res) => {
 
     pool.getConnection((err, connection) => {
         if (err) return res.status(500).json(err)
-        connection.query('delete from categories where category = ?', category, (err, results) => {
+        connection.query('DELETE FROM categories WHERE category = ?', category, (err, results) => {
             connection.release()
             if (err) return res.status(500).json(err)
             if (!(results['affectedRows'] > 0)) return res.status(404).json({
